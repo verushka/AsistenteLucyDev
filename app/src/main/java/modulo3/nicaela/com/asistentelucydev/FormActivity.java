f8ac9be0d4c4b6f3b3615a3b558bfe2b5b6e7abf
gabriel <gabitosoft@gmail.com>
2016-11-19 19:00:41 -0400

    Adding voice recognition functionality

---
+++ app/src/main/java/modulo3/nicaela/com/asistentelucydev/FormActivity.java
@@ -0,0 +1,92 @@
+package modulo3.nicaela.com.asistentelucydev;
+
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class FormActivity extends AppCompatActivity {
+
+    //Speaking variables
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+    private EditText originalText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        final Context context = this;
+        originalText = (EditText) findViewById(R.id.editFirstName);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Context context = getApplicationContext();
+//                CharSequence text = "Hello toast!";
+//                int duration = Toast.LENGTH_SHORT;
+//
+//                Toast toast = Toast.makeText(context, text, duration);
+//                toast.show();
+                try {
+
+                    promptSpeechInput();
+                } catch (Exception ex) {
+
+                    ex.printStackTrace();
+                    Toast.makeText(context, ex.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    //textSpeak.setText(result.get(0));
+                    originalText.setText(result.get(0));
+                    //processCommand(result.get(0));
+                }
+                break;
+            }
+        }
+    }
+
+}
--- app/src/main/java/modulo3/nicaela/com/asistentelucydev/MainActivity.java
+++ app/src/main/java/modulo3/nicaela/com/asistentelucydev/MainActivity.java
@@ -1,7 +1,12 @@
 package modulo3.nicaela.com.asistentelucydev;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.text.Normalizer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.buttonForm);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                goForm(v);
+            }
+        });
+    }
+
+    public void goForm(View view)
+    {
+        Intent intent = new Intent(this, FormActivity.class);
+        startActivity(intent);
     }
 }
