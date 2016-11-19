b46c62b857b2049d3a3b1ef42149e91a2551e53e
nicaela <verushka.or@gmail.com>
2016-11-19 07:37:32 -0400

    Initial commit

---
+++ app/src/main/java/modulo3/nicaela/com/asistentelucydev/MainActivity.java
@@ -0,0 +1,13 @@
+package modulo3.nicaela.com.asistentelucydev;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
